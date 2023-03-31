3900d63e6111540728b55b72360cbfdbc1236969
malinijayabal <129495263+malinijayabal@users.noreply.github.com>
2023-03-31 01:17:57 -0700

    Add files via upload

---
+++ html/icon.cs
@@ -0,0 +1,63 @@
+body{
+    background-color: lightgray;
+    margin: 0;
+}
+.icon-bars{
+    font-family:calibri,sans-serif;
+    background-color: grey;
+    width: 100px;
+    height: 100vh;
+    position: relative;
+    top: 0;
+    left: 0;
+    transform: translate(-100%);
+   
+}
+.show{
+
+    transform: translate(0%);
+   
+}
+
+    
+
+
+
+
+.loadpage{
+    position:absolute ;
+    top: 20px;
+    left: 120px;
+    font-size: 2.5rem;
+    color: grey;
+}
+
+
+.icon-bars a{
+    display: block;
+    text-align: center;
+    font-size: 2.5rem;
+    padding: 1.5rem;
+    
+}
+
+
+.icon-bars a.active{
+    background-color: seagreen;
+}
+
+
+
+
+
+
+.icon-bars a:hover{
+    background-color: black;
+}
+
+
+
+
+
+
+
---
+++ html/jsicon.js
@@ -0,0 +1,5 @@
+let loadPageEL=document.querySelector(selectors:'.loadpage');
+let iconBarsEL=document.querySelector(selectors:'.icon-bars');
+loadPageEL.addEventListener(type:'click',listener()=> {
+    iconBarsEL.classList.toggle(token:'show');
+}); 
