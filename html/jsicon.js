9cb6b741df450bee45080eedf31fa9a5ce446b11
malinijayabal <129495263+malinijayabal@users.noreply.github.com>
2023-04-10 13:00:14 +0530

    Delete html directory

--- html/jsicon.js
+++
@@ -1,5 +0,0 @@
-let loadPageEL=document.querySelector(selectors:'.loadpage');
-let iconBarsEL=document.querySelector(selectors:'.icon-bars');
-loadPageEL.addEventListener(type:'click',listener()=> {
-    iconBarsEL.classList.toggle(token:'show');
-}); 
